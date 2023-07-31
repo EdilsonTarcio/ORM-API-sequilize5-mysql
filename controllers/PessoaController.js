6fa2b52bd09c6f061d69ed2119531834e2656346
Tarcio <tarcio.ecp@gmail.com>
2023-07-30 21:15:02 -0300

    Concluindo o CRUD

--- controllers/PessoaController.js
+++ controllers/PessoaController.js
@@ -1,8 +1,10 @@
+const { where } = require('sequelize')
 const database = require('../models')
 
 class PessoaController {
   // static = evita ter que criar uma nova instancia de classe 
   // async = espera resolver os outros metodos para devolver a resposta
+  
   static async pegaTodasAsPessoas(req, res){
     // await = espera retornar os dados do banco
     // .findAll() = encontrar todas
@@ -12,6 +14,77 @@ class PessoaController {
       // .json() = converte o resiltado para o formato json
       return res.status(200).json(todasAsPessoas)  
     } catch (error){
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async pegaUmaPessoa(req, res){
+    const { id } = req.params
+    try{                                                       
+      const umaPessoa = await database.Pessoas.findOne( { 
+      //retorna um registro para encontrar todos os registros seria findAll({where:{id:Numero(id)}})
+        where: { 
+          id: Number(id) //converte em numero
+        }
+      })
+      if(umaPessoa == null){
+        return res.status(400).json({
+          message: "Pessoa não cadastrada!"
+        })
+      }else{
+        return res.status(200).json(umaPessoa)
+      }
+    }catch(error){
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async criarPessoa(req, res){
+    const novaPessoa = req.body
+    try{ 
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+      return res.status(200).json(novaPessoaCriada)
+    }catch (error){ 
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async atualizarPessoa(req, res){
+    const { id } = req.params
+    const novasInfos = req.body
+    try{ 
+      //update -> recebe 2x registros 1ª os dados 2ª o id
+      const atualizacao = await database.Pessoas.update(novasInfos, { where: { id: Number(id) } })
+      if (atualizacao == true){
+        // retorna 1 = true se achar a pessoa e conseguir atualizar
+        const pessoaAtualizada = await database.Pessoas.findOne( { 
+          //retorna um registro para encontrar todos os registros seria findAll({where:{id:Numero(id)}})
+            where: { 
+              id: Number(id) //converte em numero
+            }
+          })
+          return res.status(201).json(pessoaAtualizada)
+      }else{
+        // retorna 0 = false se não achar a pessoa e não conseguir atualizar
+        return res.status(400).json({ message: `Id ${id} não encontrado, favor verificar se a pessoa está cadastrada!`})
+      }
+    }catch (error){
+      return res.status(500).json(error.message)
+    }
+  }
+
+  static async apagaPessoas(req, res) {
+    const { id } = req.params
+    try{
+      const delecao = await database.Pessoas.destroy({ where: { id: Number(id) }})
+      if( delecao == true){
+        // retorna 1 = true se achar a pessoa e conseguir deletar
+        return res.status(200).json({ message: `id ${id}, deletado!`})
+      }else{
+        // retorna 0 = false se não achar a pessoa e não conseguir deletar
+        return res.status(400).json({ message: `id ${id} não encontrado, favor verificar se a pessoa está cadastrada!`})
+      }
+    }catch(error){
       return res.status(500).json(error.message)
     }
   }
--- routes/pessoasRoute.js
+++ routes/pessoasRoute.js
@@ -5,5 +5,12 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 //Ao acessar a rota é retornado todas as pessoas cadastradas
-
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+//Deverar acessar a rota passando o id da pesso
+router.post('/pessoas', PessoaController.criarPessoa)
+//Cria uma pessoa recebendo o body com as informações
+router.put('/pessoas/:id', PessoaController.atualizarPessoa)
+//Atualizar uma pessoa recebendo o body com as informações
+router.delete('/pessoas/:id', PessoaController.apagaPessoas)
+//Deletar uma pessoa recebendo o body com as informações
 module.exports = router
